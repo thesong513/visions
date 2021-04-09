35a977bef654ca100cd453025fe239fc2423b158
thesong513 <thesong513@163.com>
2021-04-09 15:00:39 +0800

    typing

---
+++ src/main/java/com/thesong/data/type/system/types/RelationsIterManager.java
@@ -0,0 +1,13 @@
+package com.thesong.data.type.system.types;
+
+/**
+ * @ProjectName: visions
+ * @Author: admin
+ * @DateTime: 2021/4/9 14:58
+ * @Description:
+ */
+public class RelationsIterManager {
+
+
+
+}
---
+++ src/main/java/com/thesong/data/type/system/types/VisionsBaseTypeMeta.java
@@ -0,0 +1,10 @@
+package com.thesong.data.type.system.types;
+
+/**
+ * @ProjectName: visions
+ * @Author: admin
+ * @DateTime: 2021/4/9 14:57
+ * @Description:
+ */
+public class VisionsBaseTypeMeta {
+}
